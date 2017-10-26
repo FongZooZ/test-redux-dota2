73aface75f976921807de462eb1bdb3c57e55726
FongZooZ <fongzooz1@gmail.com>
2017-10-26 14:34:44 +0700

    sample table

--- app/components/hero-table.js
+++ app/components/hero-table.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 
 class HeroesTable extends Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
-		return ();
+		let table;
+		let { heroes } = this.props;
+
+		if (heroes && heroes.length) {
+			let heroRows = [];
+			heroes.forEach((hero) => {
+				heroRows.push(
+					<tr key={`heroRow_${hero.id}`}>
+						<td>{hero.id}</td>
+						<td>{hero.name}</td>
+						<td>{hero.localized_name}</td>
+						<td>{hero.primary_attr}</td>
+						<td>{hero.attack_type}</td>
+						<td>{hero.roles.toString()}</td>
+						<td>{hero.legs}</td>
+					</tr>
+				);
+			});
+
+			table =
+				<table>
+					<thead>
+						<tr>
+							<td>ID</td>
+							<td>Name</td>
+							<td>Localized Name</td>
+							<td>Primary Attribute</td>
+							<td>Attack Type</td>
+							<td>Roles</td>
+							<td>Legs</td>
+						</tr>
+					</thead>
+					<tbody>
+						{heroRows}
+					</tbody>
+				</table>;
+		}
+
+		return (
+			<div>{table}</div>
+		);
 	}
 }
 
--- app/components/home.js
+++ app/components/home.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { fetchHeroes, removeHeroes } from '../actions/heroes';
+import HeroesTable from './hero-table';
 
 class Home extends Component {
 	render() {
-		let heroes = this.props.heroes;
-		console.log(heroes);
+		let { heroes } = this.props;
 
 		return (
 			<div id="home">
@@ -14,6 +14,7 @@ class Home extends Component {
 				<div>Select which data you want to show</div>
 				<button onClick={() => this._getHeroes()}>Get heroes</button>
 				<button onClick={() => this._removeHeroes()}>Remove heroes</button>
+				<HeroesTable heroes={heroes} />
 			</div>
 		);
 	}
